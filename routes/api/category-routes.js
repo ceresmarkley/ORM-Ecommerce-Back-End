517204f4acd0c847d992f7bf60a80ef7b99d9801
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-08-02 14:50:40 -0400

    added get routes for all categories and one category. Also created route for category creation.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,44 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const getCategories = await Category.findAll({
+        include: [{ model: Product}],
+    });
+    res.status(200).json(getCategories);
+  } catch (err) {
+    console.log(err),
+    res.status(500).json({ message: 'Internal server error!' + err});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const getCategories = await Category.findByPk(req.params.id, {
+        include: [{ model: Product}],
+    });
+    res.status(200).json(getCategories);
+  } catch (err) {
+    console.log(err),
+    res.status(500).json({ message: 'Internal server error!' + err});
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+    console.log("New Category ", newCategory, "Created!");
+  } catch (err) {
+    console.log(err),
+    res.status(500).json({ message: 'Internal server error!' + err});
+  }
 });
 
 router.put('/:id', (req, res) => {
