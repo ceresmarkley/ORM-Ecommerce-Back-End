12f2cd199c80a1de9531a996b90e0a178dc508bf
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-08-03 09:21:57 -0400

    added get all tags and get one tag by id router code.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+        include: [{ model: Product}],
+    });
+    res.status(200).json(allTags);
+  } catch (err) {
+    console.log(err),
+    res.status(500).json({ message: 'Internal server error!' + err});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const getTag = await Tag.findByPk(req.params.id, {
+        include: [{ model: Product}],
+    });
+    if (!getTag) {
+        res.status(404).json({ message: `Unable to find product with that ID!`});
+    } else {
+        res.status(200).json(getTag);
+      }
+    } catch (err) {
+      res.status(500).json({message: 'Internal server error!' + err});
+    }
 });
 
 router.post('/', (req, res) => {
